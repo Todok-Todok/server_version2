2f83d2a301f2e60b91a655b9c822f241a1257d7d
Ubuntu <ubuntu@ip-172-31-9-253.ap-northeast-2.compute.internal>
2024-09-01 23:01:36 +0000

    [FIX#2] aws ec2 서버 연결 과정 중 chromedriver 에러 픽싱

--- todokVer2/book/bestseller_book_crawling/thread_local.py
+++ todokVer2/book/bestseller_book_crawling/thread_local.py
@@ -1,10 +1,17 @@
 import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-User_Agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+#User_Agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+service = Service(executable_path='/home/ubuntu/server_version2/todokVer2/chromedriver-linux64/chromedriver')
 chrome_options = Options()
-chrome_options.add_argument(f"user-agent={User_Agent }")
+chrome_options.add_argument("--headless")
+
+# linux 환경에서 필요한 option
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+#chrome_options.add_argument(f"user-agent={User_Agent }")
 
 
 class ThreadLocalService:
@@ -14,7 +21,7 @@ class ThreadLocalService:
     def get_driver(self):
         if not hasattr(self._driver_local, 'driver'):
             # ChromeDriver 설정
-            self._driver_local.driver = webdriver.Chrome(options=chrome_options)
+            self._driver_local.driver = webdriver.Chrome(service=service, options=chrome_options)
         return self._driver_local.driver
 
     def quit_driver(self):
