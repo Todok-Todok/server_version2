7dbfc3252101d22d1445279c0fa2f132adfab7f9
Yelin Kim <121251705+yesyell@users.noreply.github.com>
2024-09-09 13:49:24 +0900

    수정

--- todokVer2/bookReview/ai/services.py
+++ todokVer2/bookReview/ai/services.py
@@ -21,11 +21,10 @@ def find_similar_keywords(keywords: List[str], all_review_keywords: List[List[st
     # 유사도가 가장 높은 서평의 인덱스 찾기
     most_similar_index = cosine_similarities.argmax()
 
-    # 가장 유사한 서평의 키워드 가져오기
-    most_similar_keywords = all_review_keywords[most_similar_index]
+    # 가장 유사한 서평과의 유사도 계산된 단어들에서 내 서평의 단어들만 추출
+    used_keywords = list(set(keywords).intersection(set(all_review_keywords[most_similar_index])))
 
     # 입력된 키워드와 가장 유사한 키워드를 포함한 모든 키워드 중에서 TF-IDF 점수를 다시 계산하여 상위 3개를 선택
-    all_keywords = keywords + most_similar_keywords
     all_keywords_str = ' '.join(all_keywords)
     all_tfidf_matrix = tfidf.fit_transform([all_keywords_str])
 
@@ -34,10 +33,8 @@ def find_similar_keywords(keywords: List[str], all_review_keywords: List[List[st
     top_indices = np.argpartition(all_tfidf_matrix[0, :].toarray().ravel(), -top_n)[-top_n:]
     top_keywords = feature_names[top_indices].tolist()
 
-    result = (top_keywords, most_similar_keywords)
-
-    # 입력 키워드와 유사한 상위 키워드를 튜플로 리턴
-    return result
+    # 입력 키워드와 유사한 서평에서 사용된 키워드를 튜플로 리턴
+    return top_keywords, used_keywords
 
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
